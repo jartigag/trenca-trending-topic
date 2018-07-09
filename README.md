c1b3f94a07ced8e4f089be58f3bd0057a3a61e2d
jartigag <jartigag@protonmail.com>
2018-07-09 18:16:11 +0200

    first commit. everything to-do :)

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # trenca-trending-topic
 just a script to collect twitter's TTs
+
+why that weird name?
+the only reason is that it fits with (this song)[https://youtu.be/7gxS0XOfz64]
---
+++ trenca.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# just a script to collect twitter's TTs
+
+__author__ = "@jartigag"
+__version__ = '0.1'
+
+#TO-DO LIST (09/07/2018)
+#TODO: everything
+
+import tweepy
+import argparse
+
+from secrets import secrets
+s = 0 # counter of the actual secret: secrets[i]
+
+def main(verbose):
+	global secrets,s
+	auth = tweepy.OAuthHandler(secrets[s]['consumer_key'], secrets[s]['consumer_secret'])
+	auth.set_access_token(secrets[s]['access_token'], secrets[s]['access_token_secret'])
+	api = tweepy.API(auth, compression=True)
+	#trendingTopics = []
+	##TODO##
+	#return trendingTopics
+
+if __name__ == '__main__':
+
+	parser = argparse.ArgumentParser(
+		description="just a script to collect twitter's TTs, v%s by @jartigag" % __version__,
+		usage="%(prog)s [-v]")
+	#parser.add_argument('arg',help='e.g.: first arg')
+	parser.add_argument('-v','--verbose',action='store_true')
+	args = parser.parse_args()
+
+	try:
+	    main(verbose)
+	except tweepy.error.TweepError as e:
+	    print("[\033[91m!\033[0m] twitter error: %s" % e)
+	    logger.error("[!] twitter error: %s" % e)
+	except Exception as e:
+	    print("[\033[91m!\033[0m] error: %s" % e)
+	    logger.error("[!] error: %s" % e)
