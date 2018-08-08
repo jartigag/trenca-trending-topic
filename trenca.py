28932dd6dbf162dfb20814a5d3e2593e10cca8f8
jartigag <jartigag@protonmail.com>
2018-08-08 20:02:16 +0200

    ft: --nTopTTs. json indentation improved

--- trenca.py
+++ trenca.py
@@ -2,13 +2,13 @@
 # -*- coding: utf-8 -*-
 # just a script to collect twitter's TTs
 #
-# usage: python3 trenca.py -cvf dbFile.json
+# usage: python3 trenca.py -cvf json dbFile.json
 
 __author__ = "@jartigag"
 __version__ = '0.1'
 
-#TO-DO LIST (24/07/2018)
-#WIP: add options to store data: [-d] in sqlite database
+#TO-DO LIST
+# - sqlite database
 
 import tweepy
 import argparse
@@ -21,11 +21,12 @@ from time import time,sleep
 from secrets import secrets
 s = 0 # counter of the actual secret: secrets[i]
 
+SLEEP_INTERVAL = 5 # secs between reqs
 woeids = json.load(open('woeids.json'))
 results = {}
 n=0 # number of api reqs
 
-def main(verbose,format,file):
+def main(verbose,format,file,nTop):
 	global secrets,s,results,n
 	try:
 
@@ -43,7 +44,11 @@ def main(verbose,format,file):
 				results[dt] = [tt[0]] #create key 'dt' and add new results list
 			if verbose:
 				print('[*]',dt,place.upper())
-				for t in tt[0]['trends']:
+				if nTop:
+					end = nTop
+				else:
+					end = len(tt[0]['trends'])-1
+				for t in tt[0]['trends'][:end]:
 					print('	','%02d'%(tt[0]['trends'].index(t)+1),'-',t['name'],
 						'(%s tweets)'%(t['tweet_volume']) if t['tweet_volume'] is not None else '')
 		if format=='json':
@@ -54,7 +59,8 @@ def main(verbose,format,file):
 	except tweepy.error.RateLimitError as e:
 		current_time = time()
 		running_time = int(current_time - init_time)
-		print("[\033[91m#\033[0m] api limit reached! \033[1m%i\033[0m api reqs were made (running time: %i secs,secrets%i)." % (n,running_time,s))
+		print("[\033[91m#\033[0m] api limit reached! \033[1m%i\033[0m api reqs were made (running time: %i secs,secrets%i)."
+			% (n,running_time,s))
 		# rotate secrets[s]
 		if s < len(secrets)-1:
 			s+=1
@@ -62,8 +68,8 @@ def main(verbose,format,file):
 			s=0
 	except tweepy.error.TweepError as e:
 		print("[\033[91m!\033[0m] twitter error: %s" % e)
-	#except Exception as e:
-	#	print("[\033[91m!\033[0m] error: %s" % e)
+	except Exception as e:
+		print("[\033[91m!\033[0m] error: %s" % e)
 
 def write_json(outFile):
 	"""
@@ -72,9 +78,16 @@ def write_json(outFile):
 	:param outFile: .json output file 
 	"""
 	with open(outFile,'w') as f:
-		print(json.dumps(results,indent=2,sort_keys=True),file=f)
+		print("[",file=f)
+		for dt in results:
+			for loc in results[dt]:
+				print("[",file=f)
+				print(json.dumps(loc,indent=2,sort_keys=True),file=f)
+				print("],",file=f) #TODO: avoid to remove last "," manually
+		print("],",file=f) #TODO: avoid to remove last "," manually
 
 def write_sqlite(outFile):
+	#TODO:
 	"""
 	write results as a sqlite database to dbFile
 
@@ -86,7 +99,6 @@ def write_sqlite(outFile):
 			 dt				TEXT,
 			 woeid			INT,
 			 location		TEXT);''')
-
 	for dt in results:
 		for loc in results[dt]:
 			conn.execute("INSERT INTO trending_topics (dt,woeid,location) \
@@ -97,22 +109,6 @@ def write_sqlite(outFile):
 					 created_at		TEXT,\
 					 locations		TEXT);")
 			conn.commit()
-
-	'''
-	conn.execute("UPDATE COMPANY set SALARY = 25000.00 where ID = 1")
-	conn.commit
-	print "Total number of rows updated :", conn.total_changes
-
-	cursor = conn.execute("SELECT id, name, address, salary from COMPANY")
-	for row in cursor:
-	   print "ID = ", row[0]
-	   print "NAME = ", row[1]
-	   print "ADDRESS = ", row[2]
-	   print "SALARY = ", row[3], "\n"
-
-	print "Operation done successfully";
-	'''
-
 	conn.close()
 
 if __name__ == '__main__':
@@ -120,39 +116,19 @@ if __name__ == '__main__':
 	parser = argparse.ArgumentParser(
 		description="just a script to collect twitter's TTs, v%s by @jartigag" % __version__,
 		usage="%(prog)s [-cdfv]")
-	parser.add_argument('-c','--continuum',action='store_true',help='run continuously')
+	parser.add_argument('-c','--continuum',action='store_true',
+		help='run continuously')
 	parser.add_argument('-v','--verbose',action='store_true')
-	parser.add_argument('-f','--format',choices=['json','sqlite'],default='json',help='format to store data')
+	parser.add_argument('-f','--format',choices=['json','sqlite'],default='json',
+		help='format to store data')
+	parser.add_argument('-n','--nTopTTs', type=int, metavar='NUMBER',
+		help='limit to NUMBER top TTs on every location')
 	parser.add_argument('file',help='output file to store data')
 	args = parser.parse_args()
 
 	if args.continuum:
 		while True:
-			main(args.verbose,args.format,args.file)
-			fifteen_mins = 60*15 # secs between reqs
-			sleep(fifteen_mins)
+			main(args.verbose,args.format,args.file,args.nTopTTs)
+			sleep(SLEEP_INTERVAL)
 	else:
-		main(args.verbose,args.format,args.file)
-
-'''
-# extract info in console (example):
-data = json.load(open('dbFile.json'))
-for d in data:
-	print('== %s =='%(d))
-	for i in range(0,len(data[d])):
-		print('*',data[d][i]['locations'][0]['name'])
-		for j in range(0,3):
-			print(' ',j+1,data[d][i]['trends'][j]['name'])
-'''
-'''
-# let's see if TTs change significantly each 5 mins:
-tts = json.load(open("dbFile.json"),object_pairs_hook=OrderedDict)
-index_tts = list(tts)
-for t in index_tts[0:5]:
-	print('[*]',t)
-	for i in range(0,len(tts[t])):
-			print(tts[t][i]['locations'][0]['name'],'-',tts[t][i]['trends'][0]['name'])
-	print()
-
-# they're nearly the same -> probably it would be better if the script retrieved less data
-'''
+		main(args.verbose,args.format,args.file,args.nTopTTs)
